0e2e6375120b007ba83d903fd6b7e19cd0699796
Nick Medveditskov <nickolaim@live.com>
2018-04-30 21:37:26 -0700

    Get CameraService

--- ros2_android_camera/src/main/java/org/ros2/android/camera/ROS2CameraActivity.java
+++ ros2_android_camera/src/main/java/org/ros2/android/camera/ROS2CameraActivity.java
@@ -1,15 +1,29 @@
 package org.ros2.android.camera;
 
 
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 
+import android.util.Log;
 import org.ros2.android.activity.ROSActivity;
 
 public class ROS2CameraActivity extends ROSActivity {
+  private final String TAG = this.getClass().getSimpleName();
 
   @Override
   public final void onCreate(final Bundle savedInstanceState) {
+    LogMessage("onCreate");
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+
+    CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+    if (manager == null) {
+      LogMessage("This device does not support Camera2 API.");
+    }
+  }
+
+  private void LogMessage(String message) {
+    Log.i(TAG, message);
   }
 }
