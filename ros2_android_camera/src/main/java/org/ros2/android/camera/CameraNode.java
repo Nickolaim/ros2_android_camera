7f2103b5c126bfcec651bd8d33017e926e4391d6
Nick Medveditskov <nickolaim@live.com>
2018-05-01 23:04:16 -0700

    Basic functionality of camera node - it tries to acquire an image from the camera, but the image is always null.

--- ros2_android_camera/src/main/java/org/ros2/android/camera/CameraNode.java
+++ ros2_android_camera/src/main/java/org/ros2/android/camera/CameraNode.java
@@ -1,13 +1,85 @@
 package org.ros2.android.camera;
 
+import android.content.Context;
+import android.graphics.ImageFormat;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.util.Log;
+import android.util.Size;
+import org.ros2.android.activity.ROSActivity;
 import org.ros2.rcljava.node.BaseComposableNode;
+import org.ros2.rcljava.publisher.Publisher;
+import org.ros2.rcljava.timer.WallTimer;
 
-public class CameraNode extends BaseComposableNode {
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.List;
 
-  private final String topic;
+class CameraNode extends BaseComposableNode {
+  private final String TAG = this.getClass().getSimpleName();
+  private final String mTopic;
+  private Publisher<sensor_msgs.msg.Image> mPublisher;
+  private final ROSActivity mActivity;
+  private WallTimer mTimer;
+  private ImageReader mImageReader;
+  private final int mWidth = 320;
+  private final int mHeight = 240;
 
-  public CameraNode(final String name, final String topic) {
+
+  CameraNode(final ROSActivity activity, final String name, final String topic) {
     super(name);
-    this.topic = topic;
+    Log.i(TAG, "CameraNode()");
+    mTopic = topic;
+    mActivity = activity;
+    this.node.createPublisher(sensor_msgs.msg.Image.class, mTopic);
+  }
+
+  void start() {
+    Log.d(TAG, "start()");
+    if (this.mTimer != null) {
+      this.mTimer.cancel();
+    }
+    setupCamera();
+    this.mTimer = node.createWallTimer(500, TimeUnit.MILLISECONDS, this::onTimer);
+  }
+
+  private void onTimer() {
+    Image img = mImageReader.acquireLatestImage();
+    if (null == img) {
+      Log.d(TAG, "onTimer: No image");
+      return;
+    }
+
+    Log.i(TAG, "onTimer: Image available");
+    sensor_msgs.msg.Image msg = new sensor_msgs.msg.Image();
+    msg.setEncoding("rgb8");
+    msg.setWidth(mWidth);
+    msg.setHeight(mHeight);
+    this.mPublisher.publish(msg);
+  }
+
+
+  private void setupCamera() {
+    Log.i(TAG, "setupCamera");
+    CameraManager manager = (CameraManager) mActivity.getSystemService(Context.CAMERA_SERVICE);
+    if (manager == null) {
+      Log.e(TAG, "This device does not support Camera2 API");
+      return;
+    }
+    try {
+      String[] cameraIdList = manager.getCameraIdList();
+      String cameraId = cameraIdList[0];
+      Log.d(TAG, "CameraId" + cameraId);
+      CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+
+      StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+      mImageReader = ImageReader.newInstance(mWidth, mHeight, ImageFormat.JPEG, 3);
+    } catch (CameraAccessException e) {
+      Log.e(TAG, "Problem with camera access", e);
+    }
   }
 }
--- ros2_android_camera/src/main/java/org/ros2/android/camera/ROS2CameraActivity.java
+++ ros2_android_camera/src/main/java/org/ros2/android/camera/ROS2CameraActivity.java
@@ -1,29 +1,43 @@
 package org.ros2.android.camera;
 
 
-import android.content.Context;
-import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 
 import android.util.Log;
 import org.ros2.android.activity.ROSActivity;
+import org.ros2.rcljava.RCLJava;
 
 public class ROS2CameraActivity extends ROSActivity {
+  private static final String RCL_JAVA_INIT_NEEDED = "RCL_JAVA_INIT_NEEDED";
   private final String TAG = this.getClass().getSimpleName();
+  private boolean mRclJavaInitNeeded = true;
 
   @Override
   public final void onCreate(final Bundle savedInstanceState) {
-    LogMessage("onCreate");
+    Log.i(TAG, "onCreate()");
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-    if (manager == null) {
-      LogMessage("This device does not support Camera2 API.");
+    if (savedInstanceState != null) {
+      mRclJavaInitNeeded = savedInstanceState.getBoolean(RCL_JAVA_INIT_NEEDED, true);
     }
+
+    if (mRclJavaInitNeeded) {
+      Log.i(TAG, "rclJavaInit");
+      RCLJava.rclJavaInit();
+      mRclJavaInitNeeded = false;
+    }
+
+    CameraNode mCameraNode = new CameraNode(this, "camera_node", "image");
+    getExecutor().addNode(mCameraNode);
+    mCameraNode.start();
   }
 
-  private void LogMessage(String message) {
-    Log.i(TAG, message);
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    if (outState != null) {
+      outState.putBoolean(RCL_JAVA_INIT_NEEDED, mRclJavaInitNeeded);
+    }
+    super.onSaveInstanceState(outState);
   }
 }
